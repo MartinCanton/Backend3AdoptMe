02696b614730725674cc6e8866c16e274f810a97
Martin Canton <martin.canton96@hotmail.com>
2024-12-26 23:39:55 -0300

    preparacion para entrega final

--- src/config/database.js
+++ src/config/database.js
@@ -2,8 +2,25 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const mongoURI = process.env.MONGO_URI;
-    await mongoose.connect(process.env.MONGO_URI);
+    let mongoURI;
+    switch(process.env.NODE_ENV){
+      case 'production':
+        mongoURI = process.env.MONGO_URI_PROD;
+        break;
+      case 'development':
+        mongoURI = process.env.MONGO_URI_DEV;
+        break;
+      case 'test':
+        mongoURI = process.env.MONGO_URI_TEST;
+        break;
+      default:
+        throw new Error('Ambiente no definido');
+    }
+
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
  
     console.log(`Conectado a la base de datos: ${mongoURI}`);
   } catch (error) {
